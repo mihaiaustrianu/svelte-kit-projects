b2c9ec7eb84a4ca9b21057eb5a66daf404973d0a
Mihai Austrianu <mihaiaustrianu@gmail.com>
2023-04-26 13:00:07 +0300

    Default global styles

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,4 +1,11 @@
 <script lang="ts">
-    import {} from "modern-normalize/modern-normalize.css"
+	import {} from 'modern-normalize/modern-normalize.css';
+	import {} from '../styles/main.scss';
 </script>
-<slot></slot>
+
+<input type="text" />
+<button>Button</button>
+<select name="" id="">
+	<option>sadas</option>
+</select>
+<slot />
